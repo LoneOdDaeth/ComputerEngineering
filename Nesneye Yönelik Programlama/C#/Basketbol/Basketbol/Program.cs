afa535fcf0e7a2b64b8cad1d5925efd889792782
Selim Oğuz Şahin <selimoguz38@outlook.com>
2022-10-31 11:13:11 +0300

    2. Sınıf

---
+++ Nesneye Yönelik Programlama/C#/Basketbol/Basketbol/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Basketbol
+{
+    class oyuncu
+    {
+        public string isimSoyisim;
+        public string mevki;
+        public double boy;
+        public string yas;
+    }
+    class basketbol
+    {
+        static void Main(string[] argc)
+        {
+            oyuncu oyuncu1 = new
+                oyuncu();
+            oyuncu oyuncu2 = new
+                oyuncu();
+
+            oyuncu1.isimSoyisim = "Kobe Bryant";
+            oyuncu1.mevki = "Kısa Forvet/Şutör Gard";
+            oyuncu1.boy = 1.98;
+            oyuncu1.yas = "Rıp";
+
+            oyuncu2.isimSoyisim = "Lebron James";
+            oyuncu2.mevki = "Uzun Forvet/Kısa Forvet";
+            oyuncu2.boy = 2.06;
+            oyuncu2.yas = "37";
+
+            Console.WriteLine("Oyuncuların ortalama boyları= " + (oyuncu1.boy + oyuncu2.boy) / 2);
+        }
+    }
+}
---
+++ Nesneye Yönelik Programlama/C#/private_class/private_class/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class oyuncu
+    {
+        private string ad; private string mevki; private int savunma;
+        private int atak; private int topSurme; private double deger;
+
+        public void setAd(string adi)
+        {
+            ad = adi;
+        }
+        public void setMevki(string mevkisi)
+        {
+            mevki = mevkisi;
+        }
+        public void setYetenek(int sav, int atk, int ts)
+        {
+            savunma = sav;
+            atak = atk;
+            topSurme = ts;
+            degerBelirle();
+        }
+        public void calis(string konu)
+        {
+            if (konu == "savunma") savunma += 1;
+            if (konu == "atak") atak += 1;
+            if (konu == "topSurme") topSurme += 1;
+
+            degerBelirle();
+        }
+        public void degerBelirle()
+        {
+            deger = savunma * 0.3 + atak * 0.4 + topSurme * 0.5;
+        }
+        public double getDeger()
+        {
+            return deger;
+        }
+
+        public int Atak
+        {
+            set 
+            {
+                if (value < 25)
+                    atak = 25;
+                else
+                    atak = value;
+                degerBelirle();
+            }
+            get
+            {
+                return atak;
+            }
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            oyuncu oyuncu1 = new oyuncu(); //1. nesne üretiliyor
+            oyuncu oyuncu2 = new oyuncu(); //2. nesne üretiliyor
+
+            oyuncu1.setAd("Oğuz");
+            oyuncu1.setMevki("Santrafor");
+            oyuncu1.setYetenek(45, 79, 81);
+
+            oyuncu2.setAd("Selim");
+            oyuncu2.setMevki("Defans");
+            oyuncu2.setYetenek(85, 59, 55);
+
+            Console.WriteLine("Oyuncu degeri:" + oyuncu1.getDeger() + " M$");
+            oyuncu1.calis("atak");
+            Console.WriteLine("Oyuncu degeri:" + oyuncu1.getDeger() + " M$");
+        }
+    }
+}
