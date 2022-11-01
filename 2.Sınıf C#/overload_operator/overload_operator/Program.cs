35e4d90d92b118071b806681ad4311014155fb03
Selim Oğuz Şahin <selimoguz38@outlook.com>
2022-11-01 14:33:26 +0300

    NesneyeYonelikProgramlama

---
+++ 2.Sınıf C#/overload_operator/overload_operator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace operatorAsiriYukleme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime basla = new DateTime();
+            basla = DateTime.Now;
+            Console.WriteLine("Çalışmaya Başlama Zamanı= " + basla.ToString());// + operatörü string birleştirme yapıyor
+            
+            for (int i = 0; i < 1000000000; i++) { }
+            
+            DateTime bitir = new DateTime();
+            bitir = DateTime.Now;
+            Console.WriteLine("Çalışmanın Bitiş Zamanı= " + bitir);// + operatörü string birleştirme yapıyor
+            
+            TimeSpan gecenSure = new TimeSpan();
+            gecenSure = bitir - basla;
+            Console.WriteLine("Toplam çalışma süresi= " + gecenSure);
+        }
+    }
+}
---
+++ 2.Sınıf C#/overload_po/overload_po/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace operatorAsiriYukleme
+{
+    class Sayi
+    {
+        private int deger;
+        public Sayi(int gelenDeger)
+        {
+            this.deger = gelenDeger;
+        }
+        public void Yazdir()
+        {
+            Console.WriteLine(this.deger);
+        }
+        public static Sayi operator +(Sayi a, Sayi b)
+        {
+            return new Sayi(a.deger + b.deger);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sayi s1 = new Sayi(3);
+            Sayi s2 = new Sayi(5);
+            Sayi s3 = s1 + s2;
+            s3.Yazdir();
+        }
+    }
+}
