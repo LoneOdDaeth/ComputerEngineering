d48ff139171404cf9c33609b896289a332eb33b8
Selim Oğuz Şahin <selimoguz38@outlook.com>
2022-11-14 01:40:20 +0300

    NesneyeYonelikProgramlama

---
+++ 2.Sınıf C#/calisma/degiskenDonusumleri/degiskenDonusumleri/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace degiskenDonusumleri
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //char a = 'T';
+            //int b = a; // ascii tablosundaki değeri döndürür
+            //Console.WriteLine(b);
+
+            //double a = 5.4;
+            //int b = (byte)a; //byte int gibi tam degerleri tutar.
+            //Console.WriteLine(b);
+        }
+    }
+}
---
+++ 2.Sınıf C#/calisma/sınıfOlusturma/sınıfOlusturma/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace program
+{
+    class player
+    {
+        public string name, position;
+        public int defence, atack, dribbling;
+        public double price;
+    }
+    class Futbol
+    {
+        static void Main(string[] args)
+        {
+            player player1 = new player();
+            player player2 = new player();
+
+            player1.name = "Berk";
+            player1.position = "Right Back";
+            player1.defence = 75;
+            player1.atack = 60;
+            player1.dribbling = 50;
+
+            player1.name = "Oğuz";
+            player1.position = "Center Attacking Midfielder";
+            player1.defence = 75;
+            player1.atack = 70;
+            player1.dribbling = 80;
+
+            Console.WriteLine("Avarage dribbels of players= " + (player1.dribbling + player2.dribbling) / 2);
+        }
+    }
+}
