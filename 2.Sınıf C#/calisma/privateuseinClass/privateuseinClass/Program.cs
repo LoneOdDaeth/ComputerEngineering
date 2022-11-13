fc168f115258d1d21d8c3c0ed695272a4278fec4
Selim Oğuz Şahin <selimoguz38@outlook.com>
2022-11-14 01:41:33 +0300

    NesneyeYonelikProgramlama(MetotIcerir)

---
+++ 2.Sınıf C#/calisma/privateuseinClass/privateuseinClass/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace basketbol
+{
+    class player
+    {
+        public string name, position;
+        private int defence, atack, dribbling; // bunlar private olduğu için bunlara değer verilemez
+        private double price; // ancak metotlarla değiştirilebilir
+
+        public void setskills(int def, int atk, int drib)
+        {
+            defence = def;
+            atack = atk;
+            dribbling = drib;
+            setprice();
+        }
+
+        public void training(string index)
+        {
+            if (index == "defence") defence += 1;
+            if (index == "atack") atack += 1;
+            if (index == "dribbling") dribbling += 1;
+            setprice();
+        }
+
+        public void setprice()
+        {
+            price = defence * 0.3 + atack * 0.4 + dribbling * 0.5;
+        }
+        
+        public double getprice()
+        {
+            return price;
+        }
+    }
+
+    class basketbol
+    {
+        static void Main(string[] args)
+        {
+            player p1 = new player();
+
+            p1.name = "lebron James";
+            p1.position = "Atack";
+            p1.setskills(75, 85, 78);
+
+            Console.WriteLine("Player price" + p1.getprice() + "M$");
+            p1.training("defence");
+            Console.WriteLine("Player price" + p1.getprice() + "M$");
+        }
+    }
+}
