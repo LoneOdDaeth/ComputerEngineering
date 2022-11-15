51ee5c31c81cbd15184f4785e9ad39d6209551db
Selim Oğuz Şahin <selimoguz38@outlook.com>
2022-11-15 10:29:07 +0300

    VeriYapilari

--- 2.Sınıf C/calisma/struct_toplu.c
+++ 2.Sınıf C/calisma/struct_toplu.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <stddef.h>
+#include <stdlib.h>
 
 struct node
 {
@@ -21,11 +21,50 @@ struct node *addlast(struct node *head, int key)
 {
     struct node *temp = (struct node *)malloc(sizeof(struct node));
     temp -> data = key;
-    temp -> next = head;
+    temp -> next = NULL;
     struct node *temp1 = head;
     while (temp1 -> next != NULL)
         temp1 = temp1 -> next;
     temp1 -> next = temp;
+    return head;
+}
+
+struct node *delete(struct node *head, int key)
+{
+    if(head == NULL)
+        printf("Liste bos");
+    struct node *temp = head;
+    if (head ->data = key)
+    {
+        head = head -> next;
+        free(temp);
+    }
+    else if (head -> next == NULL)
+        printf("Listede istediğiniz eleman yoktur");
+    else
+    {
+        while (temp -> next -> data != key)
+        {
+            if (temp -> next -> next == NULL)
+                printf("İstediginiz eleman yoktur");
+        }
+        struct node *temp1 = temp -> next;
+        temp -> next = temp -> next -> next;
+        free(temp1);
+    }
+}
+
+struct node *destroy(struct node *head)
+{
+    if(head == NULL)
+        printf("Liste bos");
+    struct node *temp;
+    while (head != NULL)
+    {
+        temp = head;
+        head = head -> next;
+        free(temp);
+    }
     return head;
 }
 
@@ -42,14 +81,15 @@ int count(struct node *head)
 
 void print(struct node *head)
 {
-    if(head == NULL)
-        printf("Listede eleman yok");
+    if (head == NULL)
+        printf("Liste bos");
     struct node *temp = head;
     while (temp != NULL)
     {
         printf("%d\t", temp -> data);
         temp = temp -> next;
     }
+
 }
 
 void listinfo(struct node *head)
@@ -59,116 +99,141 @@ void listinfo(struct node *head)
     {
         printf("%dDugumun adresi= %p", counter, head);
         printf("%dDugumun verisi= %p", counter, head -> data);
-        printf("%dDugumun bagli oldugu dugumun adresi= %p", counter, head -> next);
-        head = head -> next;
-        counter++;
+        printf("%dDugumun baglı bulundugu dugumun adresi= %p", counter, head -> next);
     }
 }
 
-struct node *destroy(struct node *head)
+//-------------------------------------------------------------------------Tek Bagli Listeler Dairesel-------------------------------------------------------------------------------//
+
+void insertFront(int key) //listenin basina eleman ekleme
 {
-    if (head == NULL)
-        printf("Liste bos");
-    struct node *temp;
-    while (head != NULL)
+    if(head == NULL)
     {
-        temp = head;
-        head = head -> next;
-        free(temp);
+        head = (struct node *)malloc(sizeof(struct node));
+        head -> data = key;
+        head -> next = head;
     }
-    return head;
+    else
+    {
+        struct node *temp = (struct node *)malloc(sizeof(struct node));
+        struct node *last = head;
+        while(last -> next != head)
+            last = last -> next;
+        temp -> next = head;
+        last -> next = temp;
+        head = temp;
+    }
 }
 
-struct node *delete(struct node *head, int key)
+void insertAtlast(int key) //listenin sonuna eleman ekleme
 {
-    if(head == NULL) //listenin bos olma durumu
-        printf("Liste bos");
+    if(head == NULL)
+    {
+        head = (struct node *)malloc(sizeof(struct node));
+        head -> data = key;
+        head -> next = head;
+    }
+    else
+    {
+        struct node *last = head;
+        while(last -> naxt != NULL)
+            last = last -> next;
+        last -> next = (struct node *)malloc(sizeof(struct node));
+        last -> next -> next = head;
+        last -> next -> data = key;
+    }
+}
+
+void concatente(struct node *list_1, struct node *list_2)
+{
+    if(list_1 == NULL)
+        list_1 = list_2;
+    else
+    {
+        struct node *last = list_1;
+        while (last -> next != list_1)
+            last = last -> next;
+        last -> next = list_2;
+        last = list_2;
+        while (last -> next != list_2)
+            last = last -> next;
+        last -> next = list_2;
+    }
+}
+
+struct node *locate(int veri, struct node *head)
+{
+    struct node *locate = NULL;
     struct node *temp = head;
-    if(head -> data == key) //listede sadece istenilen elemanın olma ihtimali
+    do
     {
-        head = head -> next;
-        free(temp);
-    }
-    else if(head -> next == NULL) //listede tek elamanın olma durumu
-    {
-        printf("Listede istediginiz eleman yok");
-        return head;
-    }
-    else //listedebirden fazla eleman bulunması ve istegimiz elemanı arama alanı
-    {
-        while (temp -> next -> data != key)
+        if(head -> data != veri)
+            head = head -> next;
+        else
         {
-            if(temp -> next -> next == NULL) //Listede birden fazla elaman bulunması ve istenilen elemaın bulunmaması durumu
-            {
-                printf("İstediginiz eleman bulunmamaktadır");
-                return head;
-            }
+            locate = head;
+            break;
         }
-        struct node *temp1 = temp -> next; //if kosulu da islemediğinde istediğimiz elemana ulaştık demektir
-        temp -> next = temp -> next -> next; //burda tempin listenin sonunu göstermesini sağlıyoruz
-        free(temp1);
+        while(head != temp)
+            return locate;
     }
-    return head;
 }
 
+//-------------------------------------------------------------------------cift bagli dogrusal-------------------------------------------------------------------------------//
 
-int main()
+void insertAtFirst(int key)
 {
-    int secim,data;
-    struct node *head = NULL;
-    while(1){
-    printf("1-Listenin Basina Eleman Ekle\n");
-    printf("2-Listenin Sonuna Eleman Ekle\n");
-    printf("3-Listeyi Gorme\n");
-    printf("4-Listeden verilen bir degere sahip dugum silmek\n");
-    printf("5-Listeyi sil\n");
-    printf("6-Listedeki eleman sayisi\n");
-    printf("7-Listenin tum eleman bilgileri\n");
-    printf("8-Programdan Cikma\n");
-    printf("Seciminiz....?");
-    scanf("%d",&secim);
-
-    switch(secim){
-
-        case 1: 
-            printf("Eklemek istediginiz degerini giriniz..?");
-            scanf("%d",&data);
-            head=addhead(head,data);
-            break;
-        
-        case 2:
-            printf("Eklemek istediginiz degerini giriniz..?");
-            scanf("%d",&data);
-            head=addlast(head,data);
-            break;
-        
-        case 3:
-            print(head);
-            break;
-        
-        case 4:
-            printf("Silmek istediginiz degerini giriniz..?");
-            scanf("%d",&data);
-            head=delete(head,data);
-            break;  
-        
-        case 5:
-            head=destroy(head);
-            break;
-        
-        case 6:
-            printf("Listede %d eleman vardir\n",count(head));
-            break;
-        
-        case 7:
-            listinfo(head);
-            break;
-        
-        case 8:
-            exit(1);
-            break;
-            default:
-            printf("Yanlis secim\n");
-       }
+    if(head == NULL)
+    {
+        head = (struct node *)malloc(sizeof(struct node));
+        head -> data = key;
+        head -> next = head;
+        head -> prev = NULL;
     }
+    else
+    {
+        struct node *temp = (struct node *)malloc(sizeof(struct node));
+        temp -> data = key;
+        temp -> next = head;
+        temp -> prev = NULL;
+        head -> prev = NULL;
+        head = temp;
+    }
+}
+
+void insertAtEnd(int key)
+{
+    if(head == NULL)
+    {
+        head = (struct node *)malloc(sizeof(struct node));
+        head -> data = key;
+        head -> next = NULL;
+        head -> prev = NULL;
+    }
+    else
+    {
+        struct node *temp = head;
+        struct node *temp2 = (struct node *)malloc(sizeof(struct node));
+        while(temp -> next != NULL)
+            temp = temp -> next;
+        temp2 -> data = key;    
+        temp2 -> next = NULL;
+        temp2 -> prev = temp;
+        temp -> next = temp2;
+    }
+}
+
+void addthen(node *other_node, node *list, int n)
+{
+    node *temp = head;
+    int i = 1;
+    while(i < n)
+    {
+        head = head -> next;
+        i++;
+    }
+    other_node -> prev = head;
+    other_node -> next = head -> next;
+    head -> next = other_node;
+    head = temp;
 }
