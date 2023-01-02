5f53c55cc72434b510b19d441e03e4a870916c9f
Selim Oğuz Şahin <selimoguz38@outlook.com>
2023-01-03 00:49:58 +0300

    VeriYapilari

--- 2.Sınıf C/calisma/denemeFinal.c
+++ 2.Sınıf C/calisma/denemeFinal.c
@@ -1,192 +1,177 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+#include <stddef.h>
 #include <conio.h>
-#include <stdbool.h>
-#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+#include <conio.h>
+#include <stdio.h>
 
-// #define SIZE 100
-// #define STACK_SIZE 100
-// #define QUEUE_SIZE 100
+#define SIZE 100
+#define STACK_SİZE 100
+#define QUEUE_SIZE 100
 
-// struct node
-// {
-//     int data;
-//     struct node *next;
-// };
+struct node
+{
+    int data;
+    struct node *next;
+};
 
-// typedef struct
-// {
-//     struct node *top;
-//     int cnt;
-// }stack;
+typedef struct
+{
+    struct node *top;
+    int cnt;
+}stack;
 
-// typedef struct
-// {
-//     int cnt;
-//     struct *front;
-//     struct node *rear;
-// }queue;
+typedef struct
+{
+    struct node *front;
+    struct node *front;
+    int cnt;
+}queue;
 
-// void initialize_stack(stack *stk)
-// {
-//     stk -> top = NULL;
-//     stk -> cnt = 0;
-// }
+void initialize_stack(stack *stk)
+{
+    stk -> top = NULL;
+    stk -> cnt = 0;
+}
 
-// void initialize_queue(queue *q)
-// {
-//     q -> ;
-//     q -> front = q -> rear =NULL;
-// }
+void initialize_queue(queue *q)
+{
+    q -> cnt = 0;
+    q -> front = q -> rear = NULL;
+}
 
-// bool isempty_stack(stack *stk)
-// {
-//     return(stk -> cnt == 0);
-// }
+bool isEmpty_stack(stack *stk)
+{
+    return(stk -> cnt == 0);
+}
 
-// bool isfull_stack(stack *stk)
-// {
-//     return(stk -> cnt == STACK_SIZE);
-// }
+bool isFull_stack(stack *stk)
+{
+    return(stk -> cnt = STACK_SİZE);
+}
 
-////////////////////////////////////////////////////// stack(yığın) ///////////////////////////////////////////////
+void push(stack *stk, int key)
+{
+    if(!isEmpty_stack(stk))
+    {
+        struct node *temp = (struct node *)malloc(sizeof(struct node));
+        temp -> data = key;
+        temp -> next = stk -> top;
+        stk -> top = temp;
+        stk -> cnt++;
+    }
+    else
+        printf("Stack dolu");
+}
 
-// void push(stack *stk, int key) //stack'e (yığına) eleman ekleme. Şeklinden dolayı yeni eleman tepeleme eklenir.
-// {
-//     if(!isfull_stack(stk))
-//     {
-//         struct node *temp = (struct node *)malloc(sizeof(struct node));
-//         temp -> data = key;
-//         temp -> next = stk -> top;
-//         stk -> top = temp;
-//         stk -> cnt++;
-//     }
-//     else
-//         printf("full stack\n");
-// }
+void pop(stack *stk)
+{
+    if(!isEmpty_stack(stk))
+    {
+        struct node *temp = stk -> top;
+        stk -> top = stk -> top -> next;
+        int key = temp -> data;
+        free(temp);
+        stk -> cnt--;
+        return x;
+    }
+}
 
-// int pop(stack *stk) //stackden eleman silme. Şeklinden dolayı tepeden eleman silinebilir.
-// {
-//     if(!isempty_stack(stk))
-//     {
-//         struct node *temp = stk -> top;
-//         stk -> top = stk -> top ->next;
-//         int x = temp -> data;
-//         free(temp);
-//         stk -> cnt--;
-//         return(x);
-//     }
-// }
+int isEmpty_queue(queue *q)
+{
+    return(q -> cnt == 0);
+}
 
-////////////////////////////////////////////////////// queue(kuyruk) ///////////////////////////////////////////////
+int isFull_queue(queue *q)
+{
+    return(q -> cnt == QUEUE_SIZE);
+}
 
-// int isempty_queue(queue *q)
-// {
-//     return(q -> cnt == 0);
-// }
+void enqueue(queue *q, int key)
+{
+    if(!isFull_queue(q))
+    {
+        struct node *temp = (struct node *)malloc(sizeof(struct node));
+        temp -> next = NULL;
+        temp -> data = key;
+        if(!isEmpty_queue(q))
+            q -> front = q -> rear ->next = temp;
+        else 
+            q -> rear = q -> rear -> next = temp;
+        q -> cnt++;
+    }
+}
 
-// int isfull_queue(queue *q)
-// {
-//     return(q -> cnt == QUEUE_SIZE)
-// }
+int dequeue(queue *q)
+{
+    if(!isEmpty_queue(q))
+    {
+        struct node *temp = q -> front;
+        int key = temp -> data;
+        q -> front = temp -> next;
+        free(temp);
+        q -> cnt--;
+        return(x);
+    }
+}
 
-// void enqueue(enqueue *q, int key) // kuyruğa eleman ekleme rear ksımana eklenmesiyle olur.
-// {
-//     if(!isfull_queue(q))
-//     {
-//         struct node *temp = (struct node *)malloc(sizeof(struct node));
-//         temp -> next = NULL;
-//         temp -> data = key;
-//         if(isempty_queue(q))
-//             q -> front
-//         else
-//             q -> rear = q -> rear -> next = temp;
-//         q -> cnt++;
-//     }
-// }
+struct node
+{
+    struct node *left;
+    struct node *right;
+    int data;
+};
 
-// int dequeue(queue *q) // kuruktan eleman çıkarma ilk elemandan itibaren olur. front kksımı
-// {
-//     if(!isempty_queue(q))
-//     {
-//         struct node *temp = q-> front;
-//         int key = temp -> data;
-//         q -> front = temp -> next;
-//         free(temp);
-//         q -> cnt--;
-//         return(x);
-//     }
-// }
+typedef struct node BTREE;
 
-// ////////////////////////////////////////////////////// tree(ağaç) ///////////////////////////////////////////////
 
-// struct node
-// {
-//     struct node *left;
-//     struct node *right;
-//     int data;
-// };
+BTREE *new_node(int key)
+{
+    BTREE *p = (struct node *)malloc(sizeof(BTREE));
+    p -> data = key;
+    p -> left = NULL;
+    p -> right = NULL;
+    return(p);
+}
 
-// typedef struct node BTREE;
+BTREE *insert(BTREE *root, int key)
+{
+    if(root != NULL)
+    {
+        if(data < root -> data)
+            root -> left = insert(root -> left, data);
+        else
+            root -> left = insert(root -> right, data);
+    }
+    else
+        root = new_node(data);
+    return(root);
+}
 
-// // yeni düğüm oluşturma, bu daha çok ağacın kökü gibi bir durumdadır.
-// // new_node
-// BTREE new_node(BTREE *root, int key)
-// {
-//     BTREE *r = (BTREE *)malloc(sizeof(BTREE));
-//     r -> data = key;
-//     r -> left = NULL;
-//     r -> right = NULL;
-//     return(r);
-// }
+void preorder(BTREE *root)
+{
+    if(root != NULL)
+    {
+        printf("%d ", root -> data);
+        preorder(root-> left);
+        preorder(root -> right);
+    }
+}
 
-// // ağaca yeni eleman ekleme, eğer küçükse sola büyükse sağa yolla.
-// // insert
-// BTREE insert(BTREE *root, int key)
-// {
-//     if(root != NULL)
-//     {
-//         if(key < root -> data)
-//             root -> left = insert(root -> left, key);
-//         else
-//             root -> right = insert(root -> right, key);
-//     }
-//     else
-//         root = new_node(key);
-//     return(root);
-// }
+void postorder(BTREE *root)
+{
+    if(root 1= NULL)
+    {
+        postorder(root -> left);
+        postorder(root -> right);
+        printf("%d ", root -> data);
+    }
+}
 
-// // sıralamalar
-// // preorder: kök sol sağ
-// void preorder(BTREE *root)
-// {
-//     if(root != NULL)
-//     {
-//         printf("%d ", root -> data);
-//         preorder(root -> left);
-//         preorder(root -> right);
-//     }
-// }
-
-// // inorder: sol kök sağ
-// void inorder(BTREE *root)
-// {
-//     if(root != NULL)
-//     {
-//         inorder(root -> left)
-//         printf("%d ", root -> data);
-//         inorder(root -> right);
-//     }
-// }
-
-// // postorder: sol sağ kök
-// void postorder(BTREE *root)
-// {
-//     if(root != NULL)
-//     {
-//         postorder(root -> left);
-//         postorder(root -> right);
-//         printf("%d ", root -> data);
-//     }
-// }
+void inorder(BTREE *root)
+{
+    if(root != NULL)
+    inorder(root -> left);
+    printf("%d ", root -> data);
+    inorder(root -> right);
+}
--- 2.Sınıf C/calisma/final.c
+++ 2.Sınıf C/calisma/final.c
@@ -1,194 +1,194 @@
-//-------------------------------------------------------------------------Kuyruklar (Queues)-------------------------------------------------------------------------------//
+-------------------------------------------------------------------------Kuyruklar (Queues)-------------------------------------------------------------------------------//
 
-// #include <stdio.h> 
-// #include <string.h>  
-// #include <stdlib.h> 
-// #include <conio.h> 
-// #include <ctype.h> 
-// #include <stdbool.h>
+#include <stdio.h> 
+#include <string.h>  
+#include <stdlib.h> 
+#include <conio.h> 
+#include <ctype.h> 
+#include <stdbool.h>
 
-// #define SIZE 100  
-// #define STACK_SIZE 100  
-// #define QUEUE_SIZE 100  
+#define SIZE 100  
+#define STACK_SIZE 100  
+#define QUEUE_SIZE 100  
 
-// struct node {  
-//     int data;  
-//     struct node * next;  
-// };
+struct node {  
+    int data;  
+    struct node *next;  
+};
 
-// typedef struct {  
-//     struct node *top;  
-//     int cnt;  
-// }stack;  
+typedef struct {  
+    struct node *top;  
+    int cnt;  
+}stack;  
 
-// typedef struct {  
-//     int cnt;  
-//     struct node *front;  
-//     struct node *rear;  
-// }queue;
+typedef struct {  
+    int cnt;  
+    struct node *front;  
+    struct node *rear;  
+}queue;
 
-// void initialize_stack(stack *stk) {  
-//     stk -> top = NULL;  
-//     stk -> cnt = 0;  
-// }
+void initialize_stack(stack *stk) {  
+    stk -> top = NULL;  
+    stk -> cnt = 0;  
+}
 
-// void initialize_queue(queue *q) {  
-//     q -> cnt = 0;   
-//     q -> front = q -> rear = NULL;  
-// } 
+void initialize_queue(queue *q) {  
+    q -> cnt = 0;   
+    q -> front = q -> rear = NULL;  
+} 
 
-// bool isEmpty_stack(stack *stk) {  
-//     return(stk -> cnt == 0);  
-// }  
+bool isEmpty_stack(stack *stk) {  
+    return(stk -> cnt == 0);  
+}  
 
-// bool isFull_stack(stack *stk) {  
-//     return(stk -> cnt == STACK_SIZE);  
-// }
+bool isFull_stack(stack *stk) {  
+    return(stk -> cnt == STACK_SIZE);  
+}
 
-// void push(stack *stk, int c) {  
-//     if(!isFull_stack(stk)) {  
-//         struct node *temp = (struct node *)malloc(sizeof(struct node));  
-//         temp -> data = c;  
-//         temp -> next = stk -> top;  
-//         stk -> top = temp;  
-//         stk -> cnt++;  
-//     } 
-//     else  
-//         printf("Stack dolu\n");  
-// }  
-// int pop(stack *stk) {  
-//     if(!isEmpty_stack(stk)) {  
-//         struct node *temp = stk -> top;  
-//         stk -> top = stk -> top -> next;  
-//         int x = temp -> data;  
-//         free(temp);  
-//         stk -> cnt--;  
-//         return x;  
-//     }  
-// }
+void push(stack *stk, int c) {  
+    if(!isFull_stack(stk)) {  
+        struct node *temp = (struct node *)malloc(sizeof(struct node));  
+        temp -> data = c;  
+        temp -> next = stk -> top;  
+        stk -> top = temp;  
+        stk -> cnt++;  
+    } 
+    else  
+        printf("Stack dolu\n");  
+}  
+int pop(stack *stk) {  
+    if(!isEmpty_stack(stk)) {  
+        struct node *temp = stk -> top;  
+        stk -> top = stk -> top -> next;  
+        int x = temp -> data;  
+        free(temp);  
+        stk -> cnt--;  
+        return x;  
+    }  
+}
 
-// int isEmpty_queue(queue *q) {  
-//     return(q -> cnt == 0);  
-// }  
+int isEmpty_queue(queue *q) {  
+    return(q -> cnt == 0);  
+}  
 
-// int isFull_queue(queue *q) {  
-//     return(q -> cnt == QUEUE_SIZE);  
-// }  
+int isFull_queue(queue *q) {  
+    return(q -> cnt == QUEUE_SIZE);  
+}  
 
-// void enqueue(queue *q, int x) {  
-//     if(!isFull_queue(q)) {  
-//         struct node *temp = (struct node *)malloc(sizeof(struct node));  
-//         temp -> next = NULL;  
-//         temp -> data = x;  
-//         if(isEmpty_queue(q))  
-//             q -> front = q -> rear = temp;  
-//     else
-//         q -> rear = q -> rear -> next = temp;  
-//     q -> cnt ++;  
-//     }  
-// }
+void enqueue(queue *q, int x) {  
+    if(!isFull_queue(q)) {  
+        struct node *temp = (struct node *)malloc(sizeof(struct node));  
+        temp -> next = NULL;  
+        temp -> data = x;  
+        if(isEmpty_queue(q))  
+            q -> front = q -> rear = temp;  
+    else
+        q -> rear = q -> rear -> next = temp;  
+    q -> cnt ++;  
+    }  
+}
 
-// int dequeue(queue *q) {  
-//     if(!isEmpty_queue(q)) {  
-//         struct node *temp = q -> front;  
-//         int x = temp -> data;  
-//         q -> front = temp -> next;  
-//         free(temp);  
-//         q -> cnt--;  
-//         return x;  
-//     }  
-// }
+int dequeue(queue *q) {  
+    if(!isEmpty_queue(q)) {  
+        struct node *temp = q -> front;  
+        int x = temp -> data;  
+        q -> front = temp -> next;  
+        free(temp);  
+        q -> cnt--;  
+        return x;  
+    }  
+}
 
-// int main() {  
-//     char ifade[SIZE];  
-//     queue q;  
-//     stack s;  
-//     int i = 0, mismatches = 0;  
-//     initialize_stack(&s);  
-//     initialize_queue(&q);  
-//     printf("Bir ifade giriniz...\n");  
-//     gets(ifade);  
-//     for(i = 0; i != strlen(ifade); i++) {  
-//         if(isalpha(ifade[i])) {  
-//             enqueue(&q, tolower(ifade[i]));  
-//             push(&s, tolower(ifade[i]));  
-//         }  
-//     }  
-//     while(!isEmpty_queue(&q)) {  
-//         if(pop(&s) != dequeue(&q)) {  
-//         mismatches = 1;  
-//         break;  
-//         }  
-//     }  
-//     if(mismatches == 1)  
-//         printf("Girdiginiz ifade palindrom degildir!\n");  
-//     else  
-//         printf("Girdiginiz ifade bir palindromdur!\n");  
-// }
+int main() {  
+    char ifade[SIZE];  
+    queue q;  
+    stack s;  
+    int i = 0, mismatches = 0;  
+    initialize_stack(&s);  
+    initialize_queue(&q);  
+    printf("Bir ifade giriniz...\n");  
+    gets(ifade);  
+    for(i = 0; i != strlen(ifade); i++) {  
+        if(isalpha(ifade[i])) {  
+            enqueue(&q, tolower(ifade[i]));  
+            push(&s, tolower(ifade[i]));  
+        }  
+    }  
+    while(!isEmpty_queue(&q)) {  
+        if(pop(&s) != dequeue(&q)) {  
+        mismatches = 1;  
+        break;  
+        }  
+    }  
+    if(mismatches == 1)  
+        printf("Girdiginiz ifade palindrom degildir!\n");  
+    else  
+        printf("Girdiginiz ifade bir palindromdur!\n");  
+}
 
-// //------------------------------------------------------------------------- AĞAÇLAR(Trees) -------------------------------------------------------------------------------//
+//------------------------------------------------------------------------- AĞAÇLAR(Trees) -------------------------------------------------------------------------------//
 
-// struct node
-// {
-//     int data;
-//     struct node *left;
-//     struct node *right;
-// };
+struct node
+{
+    int data;
+    struct node *left;
+    struct node *right;
+};
 
-// typedef struct node BTREE;
+typedef struct node BTREE;
 
-// BTREE *new_node(int key)
-// {
-//     BTREE *p = (BTREE *)malloc(sizeof(BTREE));
-//     p -> data = key;
-//     p -> left = NULL;
-//     p -> right = NULL;
-//     return(p);
-// }
+BTREE *new_node(int key)
+{
+    BTREE *p = (BTREE *)malloc(sizeof(BTREE));
+    p -> data = key;
+    p -> left = NULL;
+    p -> right = NULL;
+    return(p);
+}
 
-// BTREE *insert(BTREE *root, int data) // ağaça eleman ekleme
-// {
-//     if(root != NULL) // ilk eleman boş değilse
-//     {
-//         if(data < root -> data) // eklenecek elemanın küçük veya büyük olduğu kontrol edilir.
-//             root -> left = insert(root -> left, data); // küçükse sol tarafa
-//         else
-//             root -> right = insert(root -> right, data); // büyükse sağ tarafa
-//     }
-//     else
-//         root = new_node(data); // eleman yoksa dümdüz eklenir
-//     return(root);
-// }
+BTREE *insert(BTREE *root, int data) // ağaça eleman ekleme
+{
+    if(root != NULL) // ilk eleman boş değilse
+    {
+        if(data < root -> data) // eklenecek elemanın küçük veya büyük olduğu kontrol edilir.
+            root -> left = insert(root -> left, data); // küçükse sol tarafa
+        else
+            root -> right = insert(root -> right, data); // büyükse sağ tarafa
+    }
+    else
+        root = new_node(data); // eleman yoksa dümdüz eklenir
+    return(root);
+}
 
-// // preorder: İlk önce kök yazılır, devamında sol tarafa geçilir, eğer solda eleman yoksa sağ tarafa geçilir.
-// void preorder(BTREE *root) // preorder sıralama
-// {
-//     if(root != NULL)
-//     {
-//         printf("%d ", root -> data);
-//         preorder(root -> left);
-//         preorder(root -> right);
-//     }
-// }
+// preorder: İlk önce kök yazılır, devamında sol tarafa geçilir, eğer solda eleman yoksa sağ tarafa geçilir.
+void preorder(BTREE *root) // preorder sıralama
+{
+    if(root != NULL)
+    {
+        printf("%d ", root -> data);
+        preorder(root -> left);
+        preorder(root -> right);
+    }
+}
 
-// // inorder: sol taraf yazılır sonra kök sonra sağ taraf
-// void inorder(BTREE *root) // postorder sıralama
-// {
-//     if(root != NULL)
-//     {
-//         inorder(root -> left);
-//         printf("%d ", root -> data);
-//         inorder(root -> right);
-//     }
-// }
+// inorder: sol taraf yazılır sonra kök sonra sağ taraf
+void inorder(BTREE *root) // postorder sıralama
+{
+    if(root != NULL)
+    {
+        inorder(root -> left);
+        printf("%d ", root -> data);
+        inorder(root -> right);
+    }
+}
 
-// // postorder: sol taraf sonra sağ taraf sonra kök 
-// void postorder(BTREE *root) // postorder sıralama
-// {
-//     if(root != NULL)
-//     {
-//         postorder(root -> left);
-//         postorder(root -> right);
-//         printf("%d ",root -> data);
-//     }
-// }
+// postorder: sol taraf sonra sağ taraf sonra kök 
+void postorder(BTREE *root) // postorder sıralama
+{
+    if(root != NULL)
+    {
+        postorder(root -> left);
+        postorder(root -> right);
+        printf("%d ",root -> data);
+    }
+}
