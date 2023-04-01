b41986886702e1864ba8e4e6f38c5e6b507831a9
Selim Oğuz Şahin <selimoguz38@outlook.com>
2023-04-01 13:07:09 +0300

    C#

---
+++ 2.Sınıf C#/GorselProgramlama/Ders/timer/timer/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace timer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblsaat = new System.Windows.Forms.Label();
+            this.lbldakika = new System.Windows.Forms.Label();
+            this.lblsaniye = new System.Windows.Forms.Label();
+            this.lblsalise = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(104, 187);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 80);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "BAŞLA";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(241, 187);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(131, 80);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "TUR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(378, 187);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(134, 80);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "SET";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(177, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 26);
+            this.label3.TabIndex = 11;
+            this.label3.Text = ":";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(288, 96);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(17, 26);
+            this.label4.TabIndex = 12;
+            this.label4.Text = ":";
+            // 
+            // lblsaat
+            // 
+            this.lblsaat.AutoSize = true;
+            this.lblsaat.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblsaat.Location = new System.Drawing.Point(99, 96);
+            this.lblsaat.Name = "lblsaat";
+            this.lblsaat.Size = new System.Drawing.Size(36, 26);
+            this.lblsaat.TabIndex = 13;
+            this.lblsaat.Text = "00";
+            // 
+            // lbldakika
+            // 
+            this.lbldakika.AutoSize = true;
+            this.lbldakika.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbldakika.Location = new System.Drawing.Point(223, 96);
+            this.lbldakika.Name = "lbldakika";
+            this.lbldakika.Size = new System.Drawing.Size(36, 26);
+            this.lbldakika.TabIndex = 14;
+            this.lbldakika.Text = "00";
+            // 
+            // lblsaniye
+            // 
+            this.lblsaniye.AutoSize = true;
+            this.lblsaniye.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblsaniye.Location = new System.Drawing.Point(335, 96);
+            this.lblsaniye.Name = "lblsaniye";
+            this.lblsaniye.Size = new System.Drawing.Size(36, 26);
+            this.lblsaniye.TabIndex = 15;
+            this.lblsaniye.Text = "00";
+            // 
+            // lblsalise
+            // 
+            this.lblsalise.AutoSize = true;
+            this.lblsalise.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblsalise.Location = new System.Drawing.Point(476, 96);
+            this.lblsalise.Name = "lblsalise";
+            this.lblsalise.Size = new System.Drawing.Size(36, 26);
+            this.lblsalise.TabIndex = 17;
+            this.lblsalise.Text = "00";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(410, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 26);
+            this.label2.TabIndex = 16;
+            this.label2.Text = ":";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 28;
+            this.listBox1.Location = new System.Drawing.Point(562, 93);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(237, 172);
+            this.listBox1.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(833, 346);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.lblsalise);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblsaniye);
+            this.Controls.Add(this.lbldakika);
+            this.Controls.Add(this.lblsaat);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblsaat;
+        private System.Windows.Forms.Label lbldakika;
+        private System.Windows.Forms.Label lblsaniye;
+        private System.Windows.Forms.Label lblsalise;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ 2.Sınıf C#/GorselProgramlama/Ders/timer/timer/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace timer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int sls, sn, dk, sa;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            sls++;
+            if (sls == 59)
+            {
+                sls = 0;
+                sn++;
+                lblsaniye.Text = sn.ToString();
+
+                if (sn == 59)
+                {
+                    sn = 0;
+                    dk++;
+                    lbldakika.Text = dk.ToString();
+
+                    if (dk == 59)
+                    {
+                        dk = 0;
+                        sa++;
+                        lblsaat.Text = sa.ToString();
+                    }
+                }
+            }
+
+            lblsalise.Text = sls.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled == false)
+            {
+                timer1.Enabled = true;
+                button1.Text = "DURAKLAT";
+            }
+            else
+            {
+                timer1.Enabled = false;
+                button1.Text = "DEVAM ETTİR";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(lblsaat.Text + ":" + lbldakika.Text + ":" + lblsaniye.Text + ":" + lblsalise.Text);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            sifirla();
+            timer1.Enabled = false;
+            listBox1.Items.Clear();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            sifirla();
+        }
+        public void sifirla()
+        {
+            sls = 0;
+            sn = 0;
+            dk = 0;
+            sa = 0;
+            lblsaat.Text = "00";
+            lblsalise.Text = "00";
+            lblsaniye.Text = "00";
+            lbldakika.Text = "00";
+        }
+    }
+}
